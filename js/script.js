a71206b0d808d7a3c8d643ac7053f6a132ebdac7
Alex Corsa <98650988+Alex-Corsa@users.noreply.github.com>
2022-05-25 23:54:49 +0300

    hw-12 cruisers

--- js/script.js
+++ js/script.js
@@ -1,5 +1,48 @@
-/* Tasc №
-Условие:
+// Условие
 
-Решение:
-*/
+var cruisers = ['1-b', '1-c', '1-d', '1-e', '1-f', '1-g', '2-c', '2-e', '3-d', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ];
+var field = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+
+// Поле битвы
+
+function start(){
+    var div = document.createElement('div');
+    var body = document.querySelector('body');
+
+    div.className = 'wrap';
+    body.append(div)
+
+    for (var i = 0; i < field.length; i++) {
+        for (var m = 0; m < field.length; m++) {
+            createBtn('' + (m + 1) + '-' + field[i]);
+        }
+    }
+}
+
+// Ячейки
+
+function createBtn(text){
+    var btn = document.createElement('button');
+    btn.className = 'button' + ' click';
+    btn.innerHTML = text;
+    var wrap = document.querySelector('.wrap');
+    wrap.append(btn)
+}
+
+// Действие
+
+document.addEventListener('click', function (e){
+    var elem = e.target;
+    var check = elem.matches('.click');
+    if (check) {
+        if (cruisers.includes(elem.textContent)){
+            elem.classList.add('hit')
+            alert('Крейсер "Москва" совершил отрицательное всплытие!')
+        } else {
+            elem.classList.add('past')
+        }
+        elem.classList.remove('click')
+    }
+})
+
+start()
