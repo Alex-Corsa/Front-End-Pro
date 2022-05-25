8dc1a6fe8f7c11aa9f446ea48df41716abbe3ba5
Alex Corsa <98650988+Alex-Corsa@users.noreply.github.com>
2022-05-25 21:39:03 +0300

    hw-9 alarm

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-/* Tasc №
+/* Tasc №-1
 Условие:
-
+Мы должны ввести время в формате (10:20, чч:мм). 
+На странице отображаются часы с текущим временем. 
+При достижении указанного времени выводим сообщение «Звонок»
 Решение:
 */
+
+var tSet = prompt('Введіть час у форматі гг:хх');
+var time = tSet.split(':');
+
+var alarm = setInterval(function (){
+    var trash = new Date(),
+    tSet2 = trash.toLocaleTimeString(),
+    time2 = tSet2.split(':');
+
+    document.getElementById('alarm').innerHTML = tSet2;
+
+    if (time[0] == time2[0] && time[1] == time2[1]){
+        alert('Час прокидатися')
+        return clearInterval(alarm)
+    }
+}, 1000);
