454ff502dd8211c4ba5cec03f915b9776b71bf8b
Alex Corsa <98650988+Alex-Corsa@users.noreply.github.com>
2022-09-17 23:16:37 +0300

    hw-19(IT)

---
+++ js/All_IT.js
@@ -0,0 +1,34 @@
+class All_IT {
+    constructor(params) {
+        
+    }
+}
+
+
+
+/*
+специалист, направление
+
+язык програмирования(язык)
+
+Данный специалист предпочитает писать на .
+Данный специалист не пишет на .
+
+
+class Animal {
+  constructor(specie, favoriteMeals) {
+    this.specie = specie;
+    this.favoriteMeals = favoriteMeals;
+  }
+
+  eat(meal) {
+    return this.favoriteMeals.includes(meal)
+      ? `The ${this.specie} eat ${meal}`
+      : `The ${this.specie} do not eat ${meal}`;
+  }
+
+  makeNoise(sound) {
+    return `The ${this.specie} makes noise - ${sound}.`;
+  }
+}
+*/
